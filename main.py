a4424612669f2ff87c23d0d5b903dc857bd83078
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-08-02 00:59:18 +0530

    Added date time now functionalities and writing the text file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import selectorlib
+from datetime import datetime
 
 URL = "http://programmer100.pythonanywhere.com/"
 HEADERS = {
@@ -14,11 +15,19 @@ def scrape(url):
 
 def extract(source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(source)
+    value = extractor.extract(source)["temperature"]
     return value
+
+
+def store(extracted):
+    now = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    with open("temperatures.txt", "a") as file:
+        line = f"{now}, {extracted}\n"
+        file.write(line)
 
 
 if __name__ == "__main__":
     scraped = scrape(URL)
-    # print(scraped)
-    print(extract(scraped))
+    extracted = extract(scraped)
+    print(extracted)
+    store(extracted)
